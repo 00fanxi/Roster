5950b4e78a26a126e8053a191b54359c746bdb75
00fanxi <90121162+00fanxi@users.noreply.github.com>
2021-10-12 00:00:44 +0900

    Add files via upload

--- roster.js
+++ roster.js
@@ -1,4 +1,156 @@
-function deleteall(){
+let starter = []
+let sc = []
+let sr = []
+
+
+
+
+function getstarter(){
+	starter.splice(0,27);
+	sc.splice(0,27);
+	sr.splice(0,27);
+	var i =0;
+	while(i < 7){
+		var h = 0;
+		while(h<4){
+			starter.push(document.querySelector('.table'+i+' .row'+h).value);
+			sc.push(i);
+			sr.push(h);
+			h = h+1;
+		}
+		i=i+1;
+	}
+}
+
+function autocheck(){
+	//일단 다 지우고 시작
+	var b = 0;
+	while(b<12){
+		document.querySelector('.n'+b+' .times').style.backgroundColor = '#be9c91'
+		b=b+1;
+	}
+	//명단에 있는 근무자의 총 수를 구한다.
+	var i = 0;
+	var t = 0;
+	while(i<12){
+		if(document.querySelector('.name'+i).value !=''){
+			t=t+1;
+		}
+		i=i+1;
+	}
+	//근무자가 있는 근무수 칸의 값을 다 더한다.
+	var f = 0;
+	var a = 0;
+	while(f<12){
+		if(document.querySelector('.name'+f).value !=''){
+			a=a+Math.floor(document.querySelector('.n'+f+' .times').value);
+		}
+		f=f+1;
+	}
+	//근무표에 내용이 있는 칸이 있으면 개당 1씩 근무수 총합에 다시 더한다.
+	var c=0;
+	while(c<7){
+		var r=0;
+		while(r<4){
+			if(document.querySelector('.table'+c+' .row'+r).value!=""){
+				a=a+1;
+			}
+		r=r+1;
+		}
+	c=c+1;
+	}
+	//근무수 총합이 28일 경우 녹색, 그렇지 않을 경우 홍색을 근무자가 있는 근무수 칸에 칠한다.
+	if(a==28){
+		var i=0;
+		while(i<12){
+			if(document.querySelector('.name'+i).value !=''){
+				document.querySelector('.n'+i+' .times').style.backgroundColor='green';
+			}
+			i=i+1;
+		}
+	}else{
+		var i=0;
+		while(i<12){
+			if(document.querySelector('.name'+i).value !=''){
+				document.querySelector('.n'+i+' .times').style.backgroundColor='red';
+			}
+			i=i+1;
+		}
+	}
+}
+
+function autocount(){
+	var b = 0;
+	while(b<12){
+		document.querySelector('.n'+b+' .times').value =''
+		b=b+1;
+	}
+	
+	var i = 0;
+	var t = 0;
+	while(i<12){
+		if(document.querySelector('.name'+i).value !=''){
+			t=t+1;
+		}
+		i=i+1;
+	}
+	
+	var h = 0;
+	while(h<12){
+		if(document.querySelector('.name'+h).value!=''){
+			document.querySelector('.n'+h+' .times').value=Math.floor(28/t)
+		}
+		h=h+1;
+	}
+	
+	var d = 11
+	var j = 28-t*Math.floor(28/t);
+	var g = 0;
+	while(g<j){
+		if(document.querySelector('.name'+d).value != ''){
+			document.querySelector('.n'+d+' .times').value=Math.floor(document.querySelector('.n'+d+' .times').value)+1;
+			g=g+1;
+		}
+		d=d-1;
+	}
+	
+	//이미 수동으로 근무표에 적혀진 이름이 있으면 각 이름의 times에서 그 출현 횟수 만큼 뺀다.
+	var c=0;
+	while(c<7){
+		var r=0;
+		while(r<4){
+			if(document.querySelector('.table'+c+' .row'+r).value!=""){
+				var u = 0;
+				while(u<12){
+					if(document.querySelector('.name'+u).value==document.querySelector('.table'+c+' .row'+r).value){
+						document.querySelector('.n'+u+' .times').value=Math.floor(document.querySelector('.n'+u+' .times').value)-1;
+					}
+				u = u+1;
+				}
+			}
+		r=r+1;
+		}
+	c=c+1;
+	}
+}
+
+function deletenames(){
+	var i = 0;
+	while(i<12){
+		document.querySelector('.name'+i).value=''
+		i=i+1;
+	}
+}
+
+function deletetimes(){
+	var b = 0;
+	while(b<12){
+		document.querySelector('.n'+b+' .times').value =''
+		b=b+1;
+	}
+}
+
+function deleteroster2(){
 	var k = 0;
 	while(k<7){
 		var i = 0;
@@ -11,27 +163,60 @@ function deleteall(){
 		}
 		k=k+1;
 	}
+	
+	var i = 0;
+	while(i<28){
+		if(starter[i]==''){
+		}else{
+			document.querySelector('.table'+sc[i]+' .row'+sr[i]).value=starter[i];
+		}
+		i=i+1;
+	}
 		
 }
 
-function countall(){
-	var i = 0;
-	var b = 0;
-	while(i<12){
-		b=b+Math.floor(document.querySelector('.n'+i+' .times').value)
-		i=i+1;
-	}
-	var g = 0;
-	while(g<12){
-		if(b==28){
-		document.querySelector('.n'+g+' .times').style.backgroundColor='green';
-		}else{
-			document.querySelector('.n'+g+' .times').style.backgroundColor='red';
+function deleteroster1(){
+	var k = 0;
+	while(k<7){
+		var i = 0;
+		while(i<4){
+			if(document.querySelector('.table'+k+' .row'+i).value != ''){
+				document.querySelector('.table'+k+' .row'+i).value =''
+			}else{
+			}
+			i=i+1;	
 		}
-		g=g+1;
+		k=k+1;
 	}
 }
-
+	
+function finalcheck(){
+	var final =[]
+	var p = 0;
+	var j = 0;
+	while(p<12){
+		var i =0;
+		while(i < 7){
+			var h = 0;
+			while(h<4){
+				final.push(document.querySelector('.table'+i+' .row'+h).value);
+				h = h+1;
+			}
+			i=i+1;
+		}
+		while(final.indexOf(document.querySelector('.name'+p).value)==-1){
+			final.splice(final.indexOf(document.querySelector('.name'+p).value),1);
+			j=j+1
+		}
+		if(document.querySelector('.name'+p).value!=''){
+			if(document.querySelector('.n'+p+' .times').value!=j){
+				alert('작성하신 근무 수와 랜덤으로 배정')
+			}
+		}
+		
+	}
+	
+}
 //////////////////////////////////////////////////////////////////
 
 
@@ -52,7 +237,7 @@ function get(){
 	}
 	
 	while(terminate<1){
-		if(cant==500){
+		if(cant==300){
 			terminate = 1;
 			alert('해당 인원으로는 조건을 만족하는 근무표 생성이 불가능 합니다.')
 		}
@@ -87,14 +272,32 @@ function get(){
 				while(namelist.indexOf("") != -1){
 				namelist.splice(namelist.indexOf(""),1);  
 				}
-				//namelist에서 같은 행의 이름 제거
-				var i = 0;
-				while(i<7){
-					if(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value) !=-1){
-						namelist.splice(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value),1);
-						i=i+1;
-					}else{
-						i=i+1;
+				
+				//그전에 체크 박스에 체크되어 있으면 옵션으로 2~3번까지 중복 허용
+				if(document.querySelector('#double').checked){
+					var i = 0;
+					var k = 0;
+					while(i<7){
+						if(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value) !=-1){
+							k=k+1;
+								if(k>=Math.floor(document.querySelector('select').value)){
+									namelist.splice(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value),1);
+								}
+							i=i+1;
+						}else{
+							i=i+1;
+						}
+					}
+				}else{
+					//namelist에서 같은 행의 이름 제거
+					var i = 0;
+					while(i<7){
+						if(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value) !=-1){
+							namelist.splice(namelist.indexOf(document.querySelector('.table'+i+' .row'+r).value),1);
+							i=i+1;
+						}else{
+							i=i+1;
+						}
 					}
 				}
 				//namelist에서 같은 열의 이름 제거
@@ -143,8 +346,8 @@ function get(){
 					}
 				//table에 이미 이름이 작성되있는칸을 제외하고 
 				//위에서 만들어진 namelist에서 랜덤으로 이름을 해당table에 넣는다.
-				if(document.querySelector('.table'+c+' .row'+r).value==""){
-				//}else{
+				if(document.querySelector('.table'+c+' .row'+r).value!=""){
+				}else{
 					var ran = namelist[Math.floor(Math.random()*namelist.length)]
 					document.querySelector('.table'+c+' .row'+r).value=ran;
 					//timelist에서 선택된 이름을 한번 뺀다
